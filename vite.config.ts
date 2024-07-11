2dd5624b2af9cf3bcae0b15cc829242f719d36aa
Fred <frederik.koenig@posteo.de>
2024-07-11 11:29:32 +0200

    Fix TypeScript error in serviceWorkerRegistration.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    hmr: {
+      overlay: false, // Disable the overlay to prevent it from showing up
+    },
+  },
+});
