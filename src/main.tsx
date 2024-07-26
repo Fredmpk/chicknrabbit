82a5e8e54554ae91215b57e6199401baa07d094f
Fred <frederik.koenig@posteo.de>
2024-07-26 09:43:00 +0200

    adjusted routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker"; // Import the service worker
+import { StartPage } from "./components/StartPage";
+import { PhotoPage } from "./components/PhotoPage";
 
+// Define the router configuration using createBrowserRouter
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <StartPage />, // Define the StartPage component for the root path
+  },
+  {
+    path: "photo/:category",
+    element: <PhotoPage />, // Define the PhotoPage component with a dynamic segment
+  },
+]);
+
+// Render the application
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
